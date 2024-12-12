609bc00273da3d7158b8f9a99ee2528a72905ab5
Gergely Bod <bodge04@gmail.com>
2024-12-12 13:17:51 +0100

    models for connections

---
+++ app/Models/Driver.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Driver extends Model
+{
+    public function vehicle()
+    {
+        return $this->hasOne(Vehicle::class);
+    }
+    public function jobs()
+    {
+        return $this->hasMany(Job::class);
+    }
+}
