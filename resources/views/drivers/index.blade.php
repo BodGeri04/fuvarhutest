09528a32af8760fac9bd8d0f214b4b73bb7f92e5
Gergely Bod <bodge04@gmail.com>
2024-12-12 19:27:08 +0100

    if table empty

--- resources/views/drivers/index.blade.php
+++ resources/views/drivers/index.blade.php
@@ -1,34 +1,41 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
-    <h1>Munkáim</h1>
+    <div class="container">
+        <h1>Munkáim</h1>
 
-    <table class="table">
-        <thead>
-            <tr>
-                <th>Kezdő cím</th>
-                <th>Cél cím</th>
-                <th>Címzett Neve</th>
-                <th>Címzett Telefonszáma</th>
-                <th>Státusz</th>
-                <th>Műveletek</th>
-            </tr>
-        </thead>
-        <tbody>
-            @foreach($jobs as $job)
+        <table class="table">
+            <thead>
                 <tr>
-                    <td>{{ $job->starting_address }}</td>
-                    <td>{{ $job->destination_address }}</td>
-                    <td>{{ $job->recipient_name }}</td>
-                    <td>{{ $job->recipient_phone }}</td>
-                    <td>{{ $job->status }}</td>
-                    <td>
-                        <a href="{{ route('drivers.edit', $job->id) }}" class="btn btn-primary">Módosítás</a>
-                    </td>
+                    <th>Kezdő cím</th>
+                    <th>Cél cím</th>
+                    <th>Címzett Neve</th>
+                    <th>Címzett Telefonszáma</th>
+                    <th>Státusz</th>
+                    <th>Műveletek</th>
                 </tr>
-            @endforeach
-        </tbody>
-    </table>
-</div>
+            </thead>
+            <tbody>
+                @foreach ($jobs as $job)
+                    <tr>
+                        <td>{{ $job->starting_address }}</td>
+                        <td>{{ $job->destination_address }}</td>
+                        <td>{{ $job->recipient_name }}</td>
+                        <td>{{ $job->recipient_phone }}</td>
+                        <td>{{ $job->status }}</td>
+                        <td>
+                            <a href="{{ route('drivers.edit', $job->id) }}" class="btn btn-primary">Módosítás</a>
+                        </td>
+                    </tr>
+                @endforeach
+            </tbody>
+            @empty($job)
+                <tbody>
+                    <td class="text-center">Jelenleg nincs munkád.
+
+                    </td>
+                </tbody>
+            @endempty
+        </table>
+    </div>
 @endsection
