16a1d14c09eab8efeb651bf7764053332a48d553
Gergely Bod <bodge04@gmail.com>
2024-12-12 13:12:40 +0100

    jobs table creation

---
+++ database/migrations/2024_12_12_121027_create_jobs_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('jobs', function (Blueprint $table) {
+            $table->id();
+            $table->string('starting_address');
+            $table->string('destination_address');
+            $table->string('recipient_name');
+            $table->string('recipient_phone');
+            $table->enum('status', ['Assigned', 'In Progress', 'Completed', 'Failed'])->default('Assigned');
+            $table->unsignedBigInteger('driver_id')->nullable();
+            $table->foreign('driver_id')->references('id')->on('drivers')->onDelete('set null');
+            $table->timestamps(); 
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('jobs');
+    }
+};
