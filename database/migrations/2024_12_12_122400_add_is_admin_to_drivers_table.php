81aee6275c2b97f8a144d35cdc53ad1aa648dd80
Gergely Bod <bodge04@gmail.com>
2024-12-12 13:29:13 +0100

    removed jobs default migration & added is_admin to drivers

---
+++ database/migrations/2024_12_12_122400_add_is_admin_to_drivers_table.php
@@ -0,0 +1,28 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('drivers', function (Blueprint $table) {
+            $table->boolean('is_admin')->default(false)->after('password'); // Alapértelmezett érték: false
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('drivers', function (Blueprint $table) {
+            $table->dropColumn('is_admin');
+        });
+    }
+};
